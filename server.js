bfaf967fc8619da264b4adc08a296ec0da4a3ef6
David-Susan-khadgi <>
2021-01-05 03:33:11 +1100

    ClientSetup and Reactstrap Note you need to change React.strictmode to React.Fragment because of legacy changes

--- server.js
+++ server.js
@@ -20,6 +20,6 @@ mongoose
 //Use Routes
 app.use('/api/items', items);
 
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => console.log(`Server started on port ${port} `));
